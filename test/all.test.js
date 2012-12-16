7e9224d3f3a6c8a3535f7a687265eabde8cea37e
Alexander Early <alexander.early@gmail.com>
2012-12-16 02:46:55 -0800

    working on CLI

--- test/all.test.js
+++ test/all.test.js
@@ -1,18 +1,18 @@
 var _ = require("lodash"),
   expect = require("expect.js"),
-  dd = require("../lib/dox-docco");
+  parser = require("../lib/dox-docco");
 
 describe("dox-docco basics", function () {
 
   it("should fail with invalid options", function (done) {
-    dd({}, function (err, data) {
+    parser({}, function (err, data) {
       expect(err.message).to.equal("No buffer specified. Aborting.");
       done();
     });
   });
 
   it("should work with empty input", function (done) {
-    dd({
+    parser({
       buffer: ""
     }, function (err, data) {
       expect(err).to.equal(null);
